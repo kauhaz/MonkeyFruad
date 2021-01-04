e0fbd85638db005c7b55fbf5c78b575c790bd6b4
Bawornsak Luachan <59905945+Bawornsak@users.noreply.github.com>
2021-01-04 14:02:59 +0700

    commit

--- Frontend/src/user/pages/login.js
+++ Frontend/src/user/pages/login.js
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 const Login = () => {
+  // return (
+  //   <div>
+  //     <Navbar />
+  //     <h1>login</h1>
+  //   </div>
+  // );
+
+// ตัวอย่างจากหนังสือ
+
+  // const myName = 'Bawornsak';
+  // return (
+  //   <div className="App">
+  //     <p>ชื่อของผมคือ: { myName } </p>
+  //   </div>
+  // );
+
+  let data01 = 20;
+  let data02 = 15;
   return (
-    <div>
-      <Navbar />
-      <h1>login</h1>
+    <div className="App">
+      <p>ผลลัพธ์จากการบวก คือ {data01 + data02} </p>
     </div>
   );
-};
+  
+  
+}
 
 export default Login;
