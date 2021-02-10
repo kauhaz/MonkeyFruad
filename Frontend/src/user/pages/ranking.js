7fa09ad2127c5880d326b08aca79dffaea12217f
Bawornsak Luachan <59905945+Bawornsak@users.noreply.github.com>
2021-02-10 19:51:07 +0700

    commit

--- Frontend/src/user/pages/ranking.js
+++ Frontend/src/user/pages/ranking.js
@@ -2,13 +2,27 @@ import React, { useEffect, useState } from "react";
 import "./ranking.css";
 import Chatbot from "../components/chatbot";
 import NavbarPage from "../components/navnew";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 const Rank = () => {
   return (
     <div>
       <NavbarPage />
       <h1 className="h1-ranking">จัดอันดับคนโกง</h1>
-      <MDBContainer>
-        
+      <MDBContainer className="container-ranking">
+        <div className="rank-sorting">
+            <select
+              as="select"
+              name="rank-sort"
+              className="rank-sort-select"
+            >
+              <option selected>
+                จำนวนครั้งที่โกงมากที่สุด
+              </option>
+              <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+              <option value="กระบี่">กระบี่ </option>
+              <option value="กาญจนบุรี">กาญจนบุรี </option>
+            </select>
+        </div>
       </MDBContainer>
       <Chatbot/>
     </div>
