53ac38898c1b5ac9df34f69d0655b9301ce98adb
plunchyy <pu.punch@hotmail.com>
2021-02-15 21:30:23 +0700

    search index complete

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -33,7 +33,6 @@ import "./app.css";
 import usercontext from "./user/context/usercontext";
 import Findthief from "./user/pages/findthief";
 import Entersearch from "./user/pages/entersearch";
-import Loading from "./user/components/loading";
 
 // ที่รวม Routh ต่างๆ
 const App = () => {
@@ -153,11 +152,6 @@ const App = () => {
           <Route path="/entersearch" exact>
             <Entersearch />
           </Route>
-
-          <Route path="/loading" exact>
-            <Loading />
-          </Route>
-
         </Switch>
       </usercontext.Provider>
     </Router>
