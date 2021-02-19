4f2cd30b94283bb01ec7976f18e158509b7ca37c
huak <komsark_@hotmail.co.th>
2021-02-19 12:17:37 +0700

    circleloading

--- Frontend/src/user/components/clipLoader.js
+++ Frontend/src/user/components/clipLoader.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ClipLoader from "react-spinners/ClipLoader";
+import Default from "@bit/joshk.react-spinners-css.ouroboro";
 
 const clipLoader = ({ loading }) => {
   return (
     <div>
       <div className="box-load">
-        <ClipLoader color="#FFCD28" loading={loading} size={99} css />
+        <Default color="#FFCD28" loading={loading} size={60} css />
       </div>
     </div>
   );
