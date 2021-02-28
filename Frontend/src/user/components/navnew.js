d73894b3fad3b8f34465aa06e3088098d5ffd2a7
sokmok000 <sokmok000@gmail.com>
2021-03-01 01:31:16 +0700

    ccs

--- Frontend/src/user/components/navnew.js
+++ Frontend/src/user/components/navnew.js
@@ -81,7 +81,7 @@ const NavbarPage = ({ SetshowDropdown, showDropdown }) => {
 
   const ok = async () => {
     try {
-      const getallthief = await axios.get(`http://localhost:7000/post/post`);
+      const getallthief = await axios.get(`http://localhost:7000/thief/thief`);
       Setsearching(getallthief.data.item);
       const getallpost = await axios.get(`http://localhost:7000/post/post`);
       Setallpost(getallpost.data.item);
