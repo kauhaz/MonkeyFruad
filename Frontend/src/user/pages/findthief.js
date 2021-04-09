ca15e9d8091701ab308104ef7b32d96ae9afb990
sokmok000 <sokmok000@gmail.com>
2021-04-10 00:05:51 +0700

    ccs

--- Frontend/src/user/pages/findthief.js
+++ Frontend/src/user/pages/findthief.js
@@ -23,7 +23,7 @@ const Findthief = () => {
 
   let location = useLocation();
   const ok = async () => {
-    const getpost = await Axios.get(`http://localhost:7000/thief/post/${uid}`);
+    const getpost = await Axios.get(`https://monkeyfruad01.herokuapp.com/thief/post/${uid}`);
     Setshow(getpost.data.item);
     SetLoading(false);
   };
