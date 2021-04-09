df3baf94c25ebc9c2ab2008386cc7256b1f87c64
Bawornsak Luachan <59905945+Bawornsak@users.noreply.github.com>
2021-04-09 17:37:18 +0700

    commit

--- Frontend/src/user/pages/ranking.js
+++ Frontend/src/user/pages/ranking.js
@@ -187,45 +187,45 @@ const Rank = () => {
         </div>
         <table className="rank-table d-none d-sm-none d-md-inline">
           <tr className="rank-column-row">
-            <th className="rank-column cell col-1 ">อันดับ</th>
-            <th className="rank-column cell col ">ชื่อ</th>
-            <th className="rank-column cell col ">นามสกุล</th>
-            <th className="rank-column cell col ">เลขที่บัญชี</th>
-            <th className="rank-column cell col ">ยอดเงินทั้งหมด</th>
-            <th className="rank-column cell col ">จำนวนครั้งที่โกง</th>
-            <th className="rank-column cell col ">วันที่โกง</th>
+            <th className="rank-column col-1 ">อันดับ</th>
+            <th className="rank-column col ">ชื่อ</th>
+            <th className="rank-column col ">นามสกุล</th>
+            <th className="rank-column col ">เลขที่บัญชี</th>
+            <th className="rank-column col ">ยอดเงินทั้งหมด</th>
+            <th className="rank-column col ">จำนวนครั้งที่โกง</th>
+            <th className="rank-column col ">วันที่โกง</th>
           </tr>
         </table>
         {TitleSort == "จำนวนครั้งที่โกงมากที่สุด" ? (
           <table className="rank-table-count d-sm-none">
             <tr className="rank-column-row">
-              <th className="rank-column cell col-1">อันดับ</th>
-              <th className="rank-column cell col">ชื่อ</th>
-              <th className="rank-column cell col">นามสกุล</th>
-              <th className="rank-column cell col">เลขที่บัญชี</th>
-              <th className="rank-column cell col">จำนวนครั้งที่โกง</th>
+              <th className="rank-column col-1">อันดับ</th>
+              <th className="rank-column col">ชื่อ</th>
+              <th className="rank-column col">นามสกุล</th>
+              <th className="rank-column col">เลขที่บัญชี</th>
+              <th className="rank-column col">จำนวนครั้งที่โกง</th>
             </tr>
           </table>
         ) : null}
         {TitleSort == "ยอดเงินที่โกงสูงสุด" ? (
           <table className="rank-table-balance d-sm-none">
             <tr className="rank-column-row">
-              <th className="rank-column cell col-1">อันดับ</th>
-              <th className="rank-column cell col">ชื่อ</th>
-              <th className="rank-column cell col">นามสกุล</th>
-              <th className="rank-column cell col">เลขที่บัญชี</th>
-              <th className="rank-column cell col">ยอดเงินทั้งหมด</th>
+              <th className="rank-column col-1">อันดับ</th>
+              <th className="rank-column col">ชื่อ</th>
+              <th className="rank-column col">นามสกุล</th>
+              <th className="rank-column col">เลขที่บัญชี</th>
+              <th className="rank-column col">ยอดเงินทั้งหมด</th>
             </tr>
           </table>
         ) : null}
         {TitleSort == "วันที่โกงล่าสุด" ? (
           <table className="rank-table-date d-sm-none">
             <tr className="rank-column-row">
-              <th className="rank-column cell col-1">อันดับ</th>
-              <th className="rank-column cell col">ชื่อ</th>
-              <th className="rank-column cell col">นามสกุล</th>
-              <th className="rank-column cell col">เลขที่บัญชี</th>
-              <th className="rank-column cell col">วันที่โกง</th>
+              <th className="rank-column col-1">อันดับ</th>
+              <th className="rank-column col">ชื่อ</th>
+              <th className="rank-column col">นามสกุล</th>
+              <th className="rank-column col">เลขที่บัญชี</th>
+              <th className="rank-column col">วันที่โกง</th>
             </tr>
           </table>
         ) : null}
@@ -235,22 +235,22 @@ const Rank = () => {
               return (
                 <table className="rank-table d-none d-sm-none d-md-inline">
                   <tr className="rank-data-row">
-                    <th className="rank-column cell col-1">{index + 1}</th>
-                    <th className="rank-column cell col">{element.name}</th>
-                    <th className="rank-column cell col Fall-crisp">
+                    <th className="rank-column col-1">{index + 1}</th>
+                    <th className="rank-column col">{element.name}</th>
+                    <th className="rank-column col Fall-crisp">
                       {element.surname}
                     </th>
-                    <th className="rank-column cell col">
+                    <th className="rank-column col">
                       {element.accountnumber}
                     </th>
-                    <th className="rank-column cell col">
+                    <th className="rank-column col">
                       {element.summoney.toLocaleString(undefined, {
                         maximumFractionDigits: 2,
                       })}
                       <span>&nbsp;บาท</span>
                     </th>
-                    <th className="rank-column cell col">{element.count}</th>
-                    <th className="rank-column cell col">
+                    <th className="rank-column col">{element.count}</th>
+                    <th className="rank-column col">
                       {moment(
                         new Date(element.wanteedon.seconds * 1000)
                       ).format("MM/DD/YYYY HH:mm")}
@@ -267,15 +267,15 @@ const Rank = () => {
                 return (
                   <table className="rank-table d-sm-none">
                     <tr className="rank-data-row">
-                      <th className="rank-column cell col-1">{index + 1}</th>
-                      <th className="rank-column cell col">{element.name}</th>
-                      <th className="rank-column cell col Fall-crisp">
+                      <th className="rank-column col-1">{index + 1}</th>
+                      <th className="rank-column col">{element.name}</th>
+                      <th className="rank-column col Fall-crisp">
                         {element.surname}
                       </th>
-                      <th className="rank-column cell col">
+                      <th className="rank-column col">
                         {element.accountnumber}
                       </th>
-                      <th className="rank-column cell col">{element.count}</th>
+                      <th className="rank-column col">{element.count}</th>
                     </tr>
                   </table>
                 );
@@ -289,15 +289,15 @@ const Rank = () => {
                 return (
                   <table className="rank-table d-sm-none">
                     <tr className="rank-data-row">
-                      <th className="rank-column cell col-1">{index + 1}</th>
-                      <th className="rank-column cell col">{element.name}</th>
-                      <th className="rank-column cell col Fall-crisp">
+                      <th className="rank-column col-1">{index + 1}</th>
+                      <th className="rank-column col">{element.name}</th>
+                      <th className="rank-column col Fall-crisp">
                         {element.surname}
                       </th>
-                      <th className="rank-column cell col">
+                      <th className="rank-column  col">
                         {element.accountnumber}
                       </th>
-                      <th className="rank-column cell col">
+                      <th className="rank-column col">
                         {element.summoney.toLocaleString(undefined, {
                           maximumFractionDigits: 2,
                         })}
@@ -316,15 +316,15 @@ const Rank = () => {
                 return (
                   <table className="rank-table d-sm-none">
                     <tr className="rank-data-row">
-                      <th className="rank-column cell col-1">{index + 1}</th>
-                      <th className="rank-column cell col">{element.name}</th>
-                      <th className="rank-column cell col Fall-crisp">
+                      <th className="rank-column col-1">{index + 1}</th>
+                      <th className="rank-column col">{element.name}</th>
+                      <th className="rank-column col Fall-crisp">
                         {element.surname}
                       </th>
-                      <th className="rank-column cell col">
+                      <th className="rank-column col">
                         {element.accountnumber}
                       </th>
-                      <th className="rank-column cell col">
+                      <th className="rank-column col">
                         {moment(
                           new Date(element.wanteedon.seconds * 1000)
                         ).format("MM/DD/YYYY HH:mm")}
